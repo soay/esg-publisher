91547d269612ace1be1c8678956ba73464dd763e
Katharina <berger@dkrz.de>
2017-03-08 08:46:18 +0100

    remove PrePARE

--- src/python/esgcet/esgcet/config/cmip6_handler.py
+++ src/python/esgcet/esgcet/config/cmip6_handler.py
@@ -15,8 +15,6 @@ import imp
 
 WARN = False
 
-from cfchecker import *
-
 PrePARE_PATH = '/usr/local/conda/envs/esgf-pub/bin/PrePARE.py'
 
 version_pattern = re.compile('20\d{2}[0,1]\d[0-3]\d')
@@ -26,14 +24,6 @@ class CMIP6Handler(BasicHandler):
     def __init__(self, name, path, Session, validate=True, offline=False):
 
         self.validator = None
-        
-        try:
-            self.validator = imp.load_source('PrePARE', PrePARE_PATH)
-        except:
-            raise ESGPublishError("Unable to load the PrePARE module, expected at %s"%PrePARE_PATH)
-
-        if self.validator is None:
-            raise ESGPublishError("Unable to load the PrePARE module, expected at %s"%PrePARE_PATH)
 
         BasicHandler.__init__(self, name, path, Session, validate=validate, offline=offline)
 
@@ -49,94 +39,7 @@ class CMIP6Handler(BasicHandler):
 
     def validateFile(self, fileobj):
         """Raise ESGInvalidMetadataFormat if the file cannot be processed by this handler."""
-        
-        if self.validator is None:
-            raise ESGPublishError("Unable to load the PrePARE module, expected at %s"%PrePARE_PATH)
-
-        validator = self.validator
-
-        f = fileobj.path
-
-        config = getConfig()
-        projectSection = 'project:'+self.name
-
-
-        min_cmor_version = config.get(projectSection, "min_cmor_version", default="0.0.0")
-
-        file_cmor_version = fileobj.getAttribute('cmor_version', None)
-        
-        if compareLibVersions(min_cmor_version, file_cmor_version):
-            debug('File %s cmor-ized at version %s, passed!"'%(f, file_cmor_version))
-            return
-
-
-        min_cf_version = config.get(projectSection, "min_cf_version", defaut="")        
-
-
-
-        if len(min_cf_version) == 0: 
-            raise ESGPublishError("Minimum CF version not set in esg.ini")
-
-        fakeversion = ["cfchecker.py", "-v", "1.0", "foo"]
-        
-        (badc,coards,uploader,useFileName,standardName,areaTypes,udunitsDat,version,files)=getargs(fakeversion)
-
-
-        CF_Chk_obj = CFChecker(uploader=uploader, useFileName=useFileName, badc=badc, coards=coards, cfStandardNamesXML=standardName, cfAreaTypesXML=areaTypes, udunitsDat=udunitsDat, version=version)
-
-        rc = CF_Chk_obj.checker(f)
-
-        if (rc > 0):
-            raise ESGPublishError("File %s fails CF check"%f)
-
-
-        table = None
-
-        try:
-            table = fileobj.getAttribute('table_id', None)
-
-        except:
-            raise ESGPublishError("File %s missing required table_id global attribute"%f)
-
-
-
-        cmor_table_path = config.get(projectSection, "cmor_table_path", defaut="")        
-
-
-        if cmor_table_path == "":
-            raise ESGPublishError("cmor_table_path not set in esg.ini")            
-
-        table_file = cmor_table_path + '/CMIP6_' + table + '.json'
-
-
-        fakeargs = [ table_file ,f]
-
-        parser = argparse.ArgumentParser(prog='esgpublisher')
-        parser.add_argument('--variable')        
-        parser.add_argument('cmip6_table', action=validator.JSONAction)
-        parser.add_argument('infile', action=validator.CDMSAction)
-        parser.add_argument('outfile',
-                nargs='?',
-                help='Output file (default stdout)',
-                type=argparse.FileType('w'),
-                default=sys.stdout)
-        args = parser.parse_args(fakeargs)
-
-
-
-#        print "About to CV check:", f
- 
-        try:
-            process = validator.checkCMIP6(args)
-            if process is None:
-                raise ESGPublishError("File %s failed the CV check - object create failure"%f)
-
-            process.ControlVocab()
-
-
-        except:
-
-            raise ESGPublishError("File %s failed the CV check"%f)
+        pass
 
     def check_pid_avail(self, project_config_section, config, version=None):
         """ Returns the pid_prefix
